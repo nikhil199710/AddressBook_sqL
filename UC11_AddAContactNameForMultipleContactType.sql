c8ffd6ea52535491be3c9d9479a0179a5d151dba
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-03 08:10:51 +0530

    SQL_QueriesForAddressBook

---
+++ UC11_AddAContactNameForMultipleContactType.sql
@@ -0,0 +1,6 @@
+
+insert into AddressBook(FirstName, LastName, Address, City, State,  Zip,PhoneNumber, Email,Type)
+values
+('neville', 'kr', 'mumbai', 'blr', 'mumbai', 4561, 8789784, 'ak@gail.com', 'Family' );
+ select * from AddressBook
+ where FirstName ='neville' and LastName ='kr';
