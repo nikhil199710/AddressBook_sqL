9fb7fd56a9ff79e8db2d83f96e4f488fd8d65d17
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-13 12:32:23 +0530

    Update UC9_IdentifyandAlter.sql

--- UC9_IdentifyandAlter.sql
+++ UC9_IdentifyandAlter.sql
@@ -1,14 +1,3 @@
 --UC9_IdentifyandAlter
-alter table AddressBook add Type varchar(250);
-update AddressBook 
-set
-Type='Family' where firstName='nikhil' and lastName='ydv';
-update AddressBook 
-set
-Type='Friends' where firstName='rahul' and lastName='kr';
-update AddressBook 
-set
-Type='Family' where firstName='nikhil' and lastName='kr';
-update AddressBook 
-set
-Type='Profession' where firstName='nikhil' and lastName='yadav';
+--Alter address_book by name and contact type
+alter table address_book add AddressBookName varchar(100),ContactType varchar(100);
