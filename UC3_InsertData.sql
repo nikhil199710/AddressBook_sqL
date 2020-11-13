8f9b6316b347210e9b0550603de94e923044793f
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-13 12:26:25 +0530

    Update UC3_InsertData.sql

--- UC3_InsertData.sql
+++ UC3_InsertData.sql
@@ -1,7 +1,8 @@
 --UC3 InserData
-insert into AddressBook
-values('nikhil','ydv','kol','kol','wb',686574,11121021,'indigo@gmail.com'),
+
+
+/*Insert data in table*/
+insert into address_book values
+('nikhil','ydv','kol','kol','wb',686574,11121021,'indigo@gmail.com'),
 ('neville','kr','mumbai','mumbai','mumbai',4561,8789784,'ak@gmail.co'),
 ('rahul','kr','blr','Blr','blr',45,74632,'gg@abc.com');
-insert into AddressBook
-values('nikhil','yadav','saltlake','kol','wb',686574,11121,'indigo@gmail.co');
