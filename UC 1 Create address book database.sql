c6000c1a1119c2c9472a2dab45186be4313b5f49
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-13 12:22:08 +0530

    Update UC 1 Create address book database.sql

--- UC 1 Create address book database.sql
+++ UC 1 Create address book database.sql
@@ -1,4 +1,4 @@
--- UC 1 Create address book database
-create database Address_BookDB;
-use Address_BookDB;
-
+/*UC 1:
+Create database*/
+create database addressBookService;
+use addressBookService;
