97045669a3d2ab9427bb49b403173b828ba0c9c5
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-13 12:25:02 +0530

    Update UC2_createTable.sql

--- UC2_createTable.sql
+++ UC2_createTable.sql
@@ -1,13 +1,14 @@
---UC 2 create table
+--UC 2:
 
-Create Table AddressBook
-(
-FirstName varchar(60) not null,
-LastName varchar(60) not null,
-Address varchar(150) not null,
-City varchar(60) not null,
-State varchar(60) not null,
-Zip  int not null,
-PhoneNumber bigint not null,
-Email varchar(100) not null
+use addressBookService;
+--Create address_book table
+create table address_book
+(FirstName varchar(100) not null,
+LastName varchar(100),
+Address varchar(500) not null,
+City varchar(50) not null,
+State varchar(50) not null,
+Zip int not null,
+PhoneNo bigint not null,
+Email varchar(250) not null
 );
